2508f72cc4829b3b3135ffa7691a01be184dacd9
Laiba <sakurakim067@gmail.com>
2026-04-29 22:12:07 +0400

    Add files via upload

---
+++ Act 6- Mouse Trails/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background(169, 139, 118);
+  
+}
+
+function draw() {
+  fill(243, 228, 201);
+  ellipse(mouseX, mouseY, 40, 40);
+}
+function mousePressed()
+{
+  background();
+}
